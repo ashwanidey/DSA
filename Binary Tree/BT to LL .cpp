3d1adabfa79cf7cfdeca1ad47ac9e009a2fe3e6c
Ashwani Dey <110251931+ashwanidey@users.noreply.github.com>
2023-06-12 08:50:03 +0530

    Create BT to LL .cpp

---
+++ Binary Tree/BT to LL .cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+    
+    void flatten(Node *root)
+    {
+        Node* curr = root;
+        while(curr){
+            if(curr->left){
+                Node* pred = curr->left;
+                while(pred->right){
+                    pred = pred-> right;
+                }
+                pred->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+                
+                
+            }
+            else
+                curr = curr->right;
+        }
+    }
+};
