6e5057bbb50a3278cc9378d3b91ec67a76dcd00a
Ashwani Dey <110251931+ashwanidey@users.noreply.github.com>
2023-07-10 13:18:18 +0530

    Update Cut Into Segments.cpp

--- DP/Cut Into Segments.cpp
+++ DP/Cut Into Segments.cpp
@@ -1,76 +1,55 @@
 #include<limits.h>
+
 int solve(int n, int x, int y, int z){
-	if(n == 0)
-	return 0;
+	if(n < 0) return INT_MIN;
+	if(n == 0) return 0;
+	
+	int xtimes = solve(n-x,x,y,z) + 1;
+	int ytimes = solve(n-y,x,y,z) + 1;
+	int ztimes = solve(n-z,x,y,z) + 1;
 
-	if(n < 0)
-	return INT_MIN;
-
-	int a = solve(n-x, x,y,z)+1;
-	int b = solve(n-y, x,y,z)+1;
-	int c = solve(n-z, x,y,z)+1;
-
-	return max(a, max(b,c));
+	return max(xtimes,max(ytimes,ztimes));
 }
-int solve2(int n, int x, int y, int z, vector<int>&dp){
-	if(n == 0)
-	return 0;
 
-	if(n < 0)
-	return INT_MIN;
+int solveMem(int n, int x, int y, int z,vector<int> &dp){
+	if(n < 0) return INT_MIN;
+	if(n == 0) return 0;
+	
+	if(dp[n] != -1) return dp[n];
+	int xtimes = solveMem(n-x,x,y,z,dp) + 1;
+	int ytimes = solveMem(n-y,x,y,z,dp) + 1;
+	int ztimes = solveMem(n-z,x,y,z,dp) + 1;
 
-	if(dp[n] != -1)
-		return dp[n];
-
-	int a = solve2(n-x, x,y,z,dp)+1;
-	int b = solve2(n-y, x,y,z,dp)+1;
-	int c = solve2(n-z, x,y,z,dp)+1;
-
-	dp[n] = max(a, max(b,c));
+	dp[n] = max(xtimes,max(ytimes,ztimes));
 	return dp[n];
 }
 
-int solve3(int n, int x, int y, int z){
-	vector<int> dp(n+1, -1);
+
+int solveTab(int n, int x, int y, int z){
+	vector<int> dp(n+1,INT_MIN);
+	
 	dp[0] = 0;
-
-	for(int i=1; i<=n; i++){
-		if(i-x >= 0 && dp[i-x] != -1)
-			dp[i] = max(dp[i], dp[i-x] + 1);
-
-		if(i-y >= 0 && dp[i-y] != -1)
-			dp[i] = max(dp[i], dp[i-y] + 1);
-
-		if(i-z >= 0 && dp[i-z] != -1)
-			dp[i] = max(dp[i], dp[i-z] + 1);
-
+	
+	for(int i = 1;i<=n;i++){
+		if(i - x >= 0) dp[i] = max(dp[i], 1 + dp[i - x]);
+		if(i - y >= 0) dp[i] = max(dp[i], 1 + dp[i - y]);
+		if(i - z >= 0) dp[i] = max(dp[i], 1 + dp[i - z]);
 	}
-	if(dp[n] < 0)
-		return 0;
-	else
-		return dp[n];
+	return dp[n];
 }
 
+
 int cutSegments(int n, int x, int y, int z) {
-	
-	//Recursion method
-	// int ans = solve(n, x, y, z);
-	// if(ans < 0)
-	// 	return 0
-	// else
-	// 	return ans;
+	// int ans = solve(n,x,y,z);
+	// if(ans < 0) return 0;
+	// else return ans;
 
-	//Recursion + memoization
-	// vector<int> dp(n+1, -1);
-	// int ans=solve2(n,x,y,z,dp);
-	// if(ans < 0)
-	// 	return 0;
-	// else
-	// 	return ans;
+	// vector<int> dp(n+1,-1);
+	// int ans = solveMem(n,x,y,z,dp);
+	// if(ans < 0) return 0;
+	// else return ans;
 
-	//Tabulation
-	return solve3(n,x,y,z);
-
-
-
+	int ans = solveTab(n,x,y,z);
+	if(ans < 0) return 0;
+	else return ans;
 }
